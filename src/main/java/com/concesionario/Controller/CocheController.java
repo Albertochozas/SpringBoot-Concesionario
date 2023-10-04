3078ad4e14277188b71b6acd2980cc5e64933e66
Albertochozas <albertochozasvsk@gmail.com>
2023-10-04 20:46:37 +0200

    .

--- src/main/java/com/concesionario/Controller/CocheController.java
+++ src/main/java/com/concesionario/Controller/CocheController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/coches")
-public class CocheController {
+public class CocheController{
     private final CocheService cocheService;
     @Autowired
     public CocheController(CocheService cocheService) {
